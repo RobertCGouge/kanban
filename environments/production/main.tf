72dde90fb500473cd0dbef877fa3e3a74b3d0d1f
Robert Gouge <robert.gouge@cgi.com>
2024-10-01 12:50:30 -0500

    here goes no thing

--- environments/production/main.tf
+++ environments/production/main.tf
@@ -1,6 +1,20 @@
 module "swarm" {
-    source = "../../modules/cloud/aws/compute/swarm"
-    private_key_path = "${path.module}/private_key.pem"
+  source           = "../../modules/cloud/aws/compute/swarm"
+  private_key_path = "${path.module}/private_key.pem"
+}
+
+module "repository_secrets" {
+  source       = "../../modules/integrations/github/secrets"
+  repository   = "kanban"
+  github_owner = "robertcgouge"
+  secrets = {
+    "PRIVATE_KEY"           = module.swarm.private_key,
+    "AWS_ACCESS_KEY_ID"     = var.aws_access_key_id,
+    "AWS_SECRET_ACCESS_KEY" = var.aws_secret_access_key,
+    "AGE_KEY"               = var.age_key,
+    "GH_PAT"                = var.gh_pat
+  }
+
 }
 
 output "swarm_ssh_command" {
